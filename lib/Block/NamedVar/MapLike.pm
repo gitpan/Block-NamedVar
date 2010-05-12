01bf329c4c1775c288ba6a2f6e87196decd3a967
Chad Granum <exodist7@gmail.com>
2010-05-12 15:56:03 +0000

    Import of EXODIST/Block-NamedVar-0.006 from CPAN.
    gitpan-cpan-distribution: Block-NamedVar
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         EXODIST/Block-NamedVar-0.006.tar.gz
    gitpan-cpan-author:       EXODIST
    gitpan-cpan-maturity:     released

--- lib/Block/NamedVar/MapLike.pm
+++ lib/Block/NamedVar/MapLike.pm
@@ -82,68 +82,7 @@ __END__
 
 =head1 NAME
 
-Block::NamedVar - Replacements for map, grep with named block variables.
-
-=head1 DESCRIPTION
-
-Gives you nmap and ngrep which are new keywords that let you do a map or grep.
-The difference is you can name the block variable instead of relying on $_. You
-can also turn custom map/grep like functions into keywords that act like nmap
-and ngrep.
-
-=head1 SYNOPSIS
-
-    #!/usr/bin/perl
-    use strict;
-    use warnings;
-
-    use Block::NamedVar qw/nmap ngrep/;
-
-    my @stuff = qw/a 1 b 2 c 3/
-    my ( @list, $count );
-
-    # grep with lexical $x.
-    @list = ngrep my $x { $x =~ m/^[a-zA-Z]$/ } @stuff;
-
-    # map with lexical $x
-    @list = nmap my $x { "updated_$x" } @stuff;
-
-    # grep with package variable $v
-    $count = ngrep our $v { $v =~ m/^[a-zA-Z]$/ } @stuff;
-
-    # grep with closure over existing $y
-    my $y;
-    $count = ngrep $y { $y =~ m/^[a-zA-Z]$/ } @stuff;
-
-    # Shortcut for lexical variable
-    # must be bareword.
-    $count = ngrep thing { $thing =~ m/^[a-zA-Z]$/ } @stuff;
-
-=head1 EXPORTED FUNCTIONS
-
-=over 4
-
-=item @out = nmap var { $var ... } @list
-
-=item @out = nmap $var { $var ... } @list
-
-=item @out = nmap my $var { $var ... } @list
-
-=item @out = nmap our $var { $var ... } @list
-
-Works just like map except you specify a variable instead of using $_.
-
-=item @out = ngrep var { $var ... } @list
-
-=item @out = ngrep $var { $var ... } @list
-
-=item @out = ngrep my $var { $var ... } @list
-
-=item @out = ngrep our $var { $var ... } @list
-
-Works just like grep except you specify a variable instead of using $_.
-
-=back
+Block::NamedVar::MapLike - Parser for ngrep and nmap
 
 =head1 AUTHORS
 
